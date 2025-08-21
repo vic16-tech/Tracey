0cc20277d6aa60c5e4a31d5d40f56951bad1d29e
Victor <victorachede@gmail.com>
2025-08-20 21:56:14 -0800

    chore: removed the useless header

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://your-website-url.com', // Replace with your actual domain
+            url: 'https://tracey-app.vercel.app', // Replace with your actual domain
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
